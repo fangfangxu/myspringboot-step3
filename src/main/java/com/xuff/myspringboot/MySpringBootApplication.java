0f51279328d45e240c6e3828624a1fc7a85b5b4a
fangfangxu <472848022@qq.com>
2020-02-17 01:24:54 +0800

    commit
    commit

---
+++ src/main/java/com/xuff/myspringboot/MySpringBootApplication.java
@@ -0,0 +1,12 @@
+package com.xuff.myspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+//SpringBoot应用的入口类
+@SpringBootApplication
+public class MySpringBootApplication {
+    public static void main(String[] args){
+        //启动SpringBoot应用
+        SpringApplication.run(MySpringBootApplication.class);
+    }
+}
